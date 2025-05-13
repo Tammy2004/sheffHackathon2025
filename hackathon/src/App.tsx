8b27dd3a60d9dff0643c6b0ad481e3f55d5e6def
Amelie <amelietdavies@gmail.com>
2025-05-13 11:30:36 +0100

    fix leaflet rendering

--- hackathon/src/App.tsx
+++ hackathon/src/App.tsx
@@ -11,17 +11,19 @@ function App() {
   // const [count, setCount] = useState(0)
 
   return (
-  <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
-    <TileLayer
-      attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-    />
-    <Marker position={[51.505, -0.09]}>
-      <Popup>
-        A pretty CSS3 popup. <br /> Easily customizable.
-      </Popup>
-    </Marker>
-  </MapContainer>
+  <>
+    <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false} id="map" >
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      <Marker position={[51.505, -0.09]}>
+        <Popup>
+          A pretty CSS3 popup. <br /> Easily customizable.
+        </Popup>
+      </Marker>
+    </MapContainer>
+  </>
   )
 }
 

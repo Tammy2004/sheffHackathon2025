0a5b2cf6b84e274a5537da4d2396df51fc22f070
Amelie <amelietdavies@gmail.com>
2025-05-13 11:52:47 +0100

    Pull in json data and draw it on map

--- hackathon/src/App.tsx
+++ hackathon/src/App.tsx
@@ -4,11 +4,24 @@
 import { MapContainer } from 'react-leaflet'
 import { TileLayer } from 'react-leaflet'
 import { Marker } from 'react-leaflet'
-import { Popup } from 'react-leaflet'
 import './App.css'
+
+import { features } from "./local_data/Libraries.json";
+import { LatLng } from 'leaflet'
+
+function flipCoordinates (coords: number[]): LatLng {
+  return new LatLng(coords[1], coords[0]);
+}
 
 function App() {
   // const [count, setCount] = useState(0)
+  const markers = features.map(({geometry}) => {
+    return(
+      <>
+        <Marker position={flipCoordinates(geometry.coordinates)}></Marker>
+      </>
+    );
+  });
 
   return (
   <>
@@ -17,11 +30,7 @@ function App() {
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      <Marker position={[51.505, -0.09]}>
-        <Popup>
-          A pretty CSS3 popup. <br /> Easily customizable.
-        </Popup>
-      </Marker>
+      {markers}
     </MapContainer>
   </>
   )
